a4f134a91b7c5dcb0a694cefb5b3e2b1950a2ece
Kunal Rai <kunalatstudy@gmail.com>
2025-11-14 03:31:52 +0530

    learned the conditionals

---
+++ week-2/the-practice/index.js
@@ -0,0 +1,33 @@
+// let age = 10;
+
+// if (age >= 65) {
+//     console.log("You get your income from your pension")
+// }
+// else if(age >= 18  || age <= 65){
+//     console.log("Each month you get a salary")
+// }
+// else{
+//     console.log("The value of the age variable is not numerical.")
+// }
+
+// using the switch statement
+
+let day = "Sunday";
+switch(day){
+    case "Monday":
+        console.log("Do Something")
+        break;
+    case "Tuesday":
+        console.log("Do Something")
+        break
+    case "Wednesday":
+        console.log("Do Something")
+    case "Thusday":
+        console.log("Do Something")
+    case "Friday":
+        console.log("Do Something")
+    case "Saturday":
+        console.log("Do Something")
+    case "Sunday":
+        console.log("Do Something")
+}
